e9855fa76c23c27db934f1b018c69bee42db775e
Anideep1 <anideepbhowmick1@gmail.com>
2024-04-09 00:12:28 +0530

    some files are added

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ db.connect(function (error) {
     console.log("Successfully connceted to db");
   }
 });
+
 //PORT Define
 server.listen(8085, function check(error) {
   if (error) {
@@ -28,6 +29,7 @@ server.listen(8085, function check(error) {
     console.log("Sucessfully started the PORT!");
   }
 });
+
 //POST Method
 server.post("/api/student/add", (req, res) => {
   let details = {
@@ -44,14 +46,50 @@ server.post("/api/student/add", (req, res) => {
     }
   });
 });
+
 //View the records
 server.get("/api/student", (req, res) => {
-  var sql = "SELECT * FROM student";
+  let sql = "SELECT * FROM student";
   db.query(sql, function (error, result) {
     if (error) {
       console.log("Error connecting to db");
     } else {
       res.send({ status: true, data: result });
+    }
+  });
+});
+
+//Search the records
+server.get("/api/student/:id", (req, res) => {
+  let studentid = req.params.id;
+  let sql = "SELECT * FROM student WHERE id=" + studentid;
+  db.query(sql, function (error, result) {
+    if (error) {
+      console.log("Error in search records");
+    } else {
+      res.send({ status: true, data: result });
+    }
+  });
+});
+
+//Update the records
+server.put("/api/student/updated/:id", (req, res) => {
+  let sql =
+    "UPDATE student SET stname=" +
+    req.body.stname +
+    ",course=" +
+    req.body.course +
+    ",fee=" +
+    req.body +
+    fee +
+    "WHERE id=" +
+    res.body.id;
+
+  let a = db.query(sql, (error, result) => {
+    if (error) {
+      res.send({ status: false, message: "Updated failed" });
+    } else {
+      res.send({ status: true, message: "Sucessfully Updated" });
     }
   });
 });
